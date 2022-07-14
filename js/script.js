c487b7ed768ebd9246e3710819abbfcea3864ba0
Sergey Goncharuk <fleshed914@gmail.com>
2022-07-14 23:03:47 +0300

    feat: add index, services, technique, contacts pages

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 
 $(document).ready(function(){
 
+    $('.cert__items').slick({
+        'slidesToShow': 4,
+        'arrows': true,
+        nextArrow: $(document).find('.cert__arrow_left'),
+        prevArrow: $(document).find('.cert__arrow_right'),
+    }); 
 
 });
 
