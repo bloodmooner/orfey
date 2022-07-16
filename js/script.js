b8a127594953b18a4cead74ccbd01b5cb3ccaad1
Sergey Goncharuk <fleshed914@gmail.com>
2022-07-16 11:18:50 +0300

    feat: add mobile menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 
 $(document).ready(function(){
 
+     // Mobile Menu START
+
+     let closeMenuButton = $(".header-mobile__close");
+     let openMenuButton = $(".header__burger");
+     let menuContainer = $(".header-mobile");
+     let menuLinks = $(".header-mobile__link");
+   
+   
+     openMenuButton.click(function() {
+       menuContainer.addClass("header-mobile_active");
+       console.log('open');
+     });
+   
+     closeMenuButton.click(function() {
+       menuContainer.removeClass("header-mobile_active");
+     });
+   
+     menuLinks.click(function() {
+       menuContainer.removeClass("header-mobile_active");
+     });
+ 
+ 
+     // Mobile Menu END
+
     $('.cert__items').slick({
         'slidesToShow': 4,
         'arrows': true,
