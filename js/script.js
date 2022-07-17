02ec90a237eb5672487e219210632116706a57be
Andrey <nazarevich.andrey@mail.ru>
2022-07-17 18:51:34 +0300

    page services

--- js/script.js
+++ js/script.js
@@ -32,5 +32,56 @@ $(document).ready(function(){
         prevArrow: $(document).find('.cert__arrow_right'),
     }); 
 
+    //Tabs toggle
+    $('ul.services__tabs').on('click', 'li:not(.services__tab_active)', function() {
+      $(this)
+        .addClass('services__tab_active').siblings().removeClass('services__tab_active')
+        .closest('div.services__content').find('div.services__tab-list').removeClass('open').eq($(this).index()).addClass('open');
+    });
+
+    //Tabs animation
+    const tabsContainer = document.querySelector(".services__tabs-container"),
+    tabs = tabsContainer.querySelectorAll("li"),
+    selector = tabsContainer.querySelector(".selector");
+
+
+    function animationSelector () {
+    let activeItem = tabsContainer.querySelector("li.active");
+    selector.style.width = activeItem.offsetWidth + "px";
+    selector.style.left = activeItem.offsetLeft + "px";
+    }
+
+    tabs.forEach(tab => {
+      tab.addEventListener("click", () => {
+        tabsContainer.querySelector("li.active").classList.remove("active");
+        tab.classList.add("active");
+        animationSelector();
+      });
+    });
+
+    animationSelector();
+
+
+
+
+    //Accordion
+    $(".services__tab-body:first").css("display", "block");
+  
+    $(".services__tab-header").click(function () {
+      
+      if ( $(this).find(".services__tab-icon").hasClass("services__tab-icon_active") ) {
+        $(this).find(".services__tab-icon").removeClass("services__tab-icon_active");
+      } else {
+        $(".services__tab-icon").removeClass("services__tab-icon_active");
+        $(this).find(".services__tab-icon").addClass("services__tab-icon_active");
+      }
+      
+
+      $(this).next().slideToggle(400);
+      $(".services__tab-body").not($(this).next()).slideUp(400); 
+    });
+
+    
+
 });
 
